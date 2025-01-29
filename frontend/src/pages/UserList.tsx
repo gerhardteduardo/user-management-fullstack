ffa1b5814b42205e493b80f900b74645830efb74
Eduardo Gerhardt <comercial.eduardogerhardt@gmail.com>
2025-01-29 15:34:17 -0300

    feat: add axios

--- frontend/src/pages/UserList.tsx
+++ frontend/src/pages/UserList.tsx
@@ -1,3 +1,4 @@
+import api from "@/services/api";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -7,17 +8,26 @@ interface User {
 
 const UserList = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    fetch("http://localhost:3000/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data))
-      .catch((error) => console.error("Erro ao buscar usuários:", error));
+    api.get("/users")
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError("Erro ao buscar usuários");
+        setLoading(false);
+      });
   }, []);
 
   return (
     <div className="  p-10 flex flex-col border-2 rounded-lg">
       <h1 className="font-black text-xl">Lista de Usuários</h1>
+      {loading && <p>Carregando...</p>}
+      {error && <p className="text-red-500">{error}</p>}
       <ul className="mt-2">
         {users.map((user) => (
           <li key={user.id} className="p-2 border-b">{user.name}</li>
