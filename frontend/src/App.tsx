b9d533e11d02b4d216320abd1be6e82c2211657f
Eduardo Gerhardt <comercial.eduardogerhardt@gmail.com>
2025-01-29 11:55:51 -0300

    feat: add frontend environment

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div className="items-center justify-center flex mt-10">
+          <h1 className="font-black text-5xl">Vite + React + Typescript + Tailwind +</h1>
+          <Button variant="destructive" className="mx-2 font-black text-xl">Shadcn/ui</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
